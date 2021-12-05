0c7efc533f9aca2327edfc6eb2b6e524745973db
alexcstd <alex.scustod@gmail.com>
2021-12-05 16:08:32 -0300

    Collision 8bit sound added

--- src/Ball/Ball.gd
+++ src/Ball/Ball.gd
@@ -12,6 +12,7 @@ func _physics_process(delta):
 	var collision_object = move_and_collide(velocity * speed * delta)
 	if collision_object:
 		velocity = velocity.bounce(collision_object.normal)
+		$CollisionSound.play()
 func stop_ball():
 	speed = 0
 func restart_ball():
