7ef1948c7423e83945b42cf758a40c9d8a1096dd
alexcstd <alex.scustod@gmail.com>
2021-12-05 00:13:27 -0300

    Level scene countdown fixed

--- src/Level/Level.gd
+++ src/Level/Level.gd
@@ -4,20 +4,27 @@ var PlayerScore = -2
 var OpponentScore = -2
 
 func _on_Left_body_entered(body):
-	$Ball.position = Vector2(640,360)
+	score_achieved()
 	OpponentScore += 1
-	get_tree().call_group("BallGroup", "stop_ball")
+	
 	
 
 func _on_Right_body_entered(body):
-	$Ball.position = Vector2(640, 360)
+	score_achieved()	
 	PlayerScore += 1
 	
 	
 func _process(delta):
 	$PlayerScore.text = str(PlayerScore)
 	$OpponentScore.text = str(OpponentScore)
+	
 
 
 func _on_CountdownTimer_timeout():
-	pass # Replace with function body.
+	get_tree().call_group("BallGroup", "restart_ball")
+	
+	
+func score_achieved():
+	$Ball.position = Vector2(640,360)
+	get_tree().call_group("BallGroup", "stop_ball")
+	$CountdownTimer.start()
