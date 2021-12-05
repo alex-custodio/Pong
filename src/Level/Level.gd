5cc353e1bfb9ef8fc6f34c220e474db8acbd2fc2
alexcstd <alex.scustod@gmail.com>
2021-12-05 16:10:12 -0300

    Area2D collision problem fixed

--- src/Level/Level.gd
+++ src/Level/Level.gd
@@ -1,7 +1,11 @@
 extends Node
 
-var PlayerScore = -2
-var OpponentScore = -2
+var PlayerScore = 0
+var OpponentScore = 0
+
+func _ready():
+	score_achieved()
+	#to make the ball stop in the beggining of the game
 
 func _on_Left_body_entered(body):
 	score_achieved()
@@ -17,14 +21,16 @@ func _on_Right_body_entered(body):
 func _process(delta):
 	$PlayerScore.text = str(PlayerScore)
 	$OpponentScore.text = str(OpponentScore)
-	
+	$CountdownLabel.text = str(int($CountdownTimer.time_left +1)) 
 
 
 func _on_CountdownTimer_timeout():
 	get_tree().call_group("BallGroup", "restart_ball")
-	
+	$CountdownLabel.visible = false
 	
 func score_achieved():
 	$Ball.position = Vector2(640,360)
 	get_tree().call_group("BallGroup", "stop_ball")
 	$CountdownTimer.start()
+	$CountdownLabel.visible = true
+	$ScoreSound.play()
